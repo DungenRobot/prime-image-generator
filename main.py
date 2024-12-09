c6e4875b07d432779ab71aa5e5f0d1bc5e289cb7
Dungen <104450078+DungenRobot@users.noreply.github.com>
2024-12-08 21:15:31 -0500

    stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import png, random
+from sympy import isprime
 
 def create_image_mask(rows, cutoff):
     img_mask: list[str] = []
@@ -13,55 +14,40 @@ def create_image_mask(rows, cutoff):
     
     return img_mask
 
-# I would definitely love to explore how this function checks if a number is prime but it is 3:22 AM
-# so I'll save that for the morning
-# https://stackoverflow.com/a/36525088
-def isPrime(n, k=5): # miller-rabin
-    #print(n % 100)
-    # n_to_str = n
-    # while n_to_str > 0:
-    #     print(n_to_str %  pow(10, 100))
-    #     n_to_str = n_to_str // pow(10, 100)
-    #print()
-
-    from random import randint
-    if n < 2: return False
-    for p in [2,3,5,7,11,13,17,19,23,29]:
-        if n % p == 0: return n == p
-    s, d = 0, n-1
-    while d % 2 == 0:
-        s, d = s+1, d//2
-    for i in range(k):
-        x = pow(randint(2, n-1), d, n)
-        if x == 1 or x == n-1: continue
-        for r in range(1, s):
-            x = (x * x) % n
-            if x == 1: return False
-            if x == n-1: break
-        else: return False
-    return True
-
-
 
 def store_number(file_name, number, width):
 
     number_as_lines = []
 
     while number > 0:
-        number_as_lines.append(str(number % pow(10, width)))
+        number_as_lines.append(number % pow(10, width))
         number = number // pow(10, width)
 
     number_as_lines.reverse()
 
     t_f_name = file_name.removesuffix('.png') + '_prime'
 
+    s = "%0" + str(width) + "d\n" #include leading 0s
 
     with open(t_f_name, 'w') as f:
 
         for line in number_as_lines:
-            f.write(line + '\n')
+            f.write(s % line)
         
     print("Saved as", t_f_name)
+
+
+def generate_number_from_masK(mask):
+    number = 0
+
+    for line in mask:
+        for char in line:
+            next_value = 1
+            if char != 'X':
+                next_value = random.choice([0, 3, 5, 6, 8])
+            number = (number * 10) + next_value
+
+    return number
 
 
 def main(f_name: str):
@@ -73,22 +59,15 @@ def main(f_name: str):
 
     img_mask: list[str] = create_image_mask(rows, pal_white)
 
-    cool_number = 15
+    cool_number = 15 #set the default to some non prime number so we don't skip the loop
 
     i = 0
 
-    while not isPrime(cool_number):
+    while not isprime(cool_number):
         print("Iteration: ", i)
-        cool_number = 0
+        cool_number = generate_number_from_masK(img_mask)
 
         i += 1
-
-        for line in img_mask:
-            for char in line:
-                next_value = 1
-                if char != 'X':
-                    next_value = random.choice([0, 3, 5, 6, 8])
-                cool_number = (cool_number * 10) + next_value
 
     print("Done in %s iterations" % i)
 
@@ -96,4 +75,4 @@ def main(f_name: str):
 
 
 if __name__ == "__main__":
-    main("example.png")
+    main("acm_sym_k_grad_pos.png")
